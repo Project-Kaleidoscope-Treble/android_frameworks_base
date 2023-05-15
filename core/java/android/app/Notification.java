839efbf87de0db0f182027667c76b798064ad941
Ioana Alexandru <aioana@google.com>
2023-05-15 16:15:55 +0000

    Check URIs in notification public version.
    Bug: 276294099
    Test: atest NotificationManagerServiceTest NotificationVisitUrisTest
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:67cd169d073486c7c047b80ab83843cdee69bf53)
    Merged-In: I670198b213abb2cb29a9865eb9d1e897700508b4
    Change-Id: I670198b213abb2cb29a9865eb9d1e897700508b4

--- core/java/android/app/Notification.java
+++ core/java/android/app/Notification.java
@@ -2765,6 +2765,10 @@ public class Notification implements Parcelable
      * @hide
      */
     public void visitUris(@NonNull Consumer<Uri> visitor) {
+        if (publicVersion != null) {
+            publicVersion.visitUris(visitor);
+        }
+
         visitor.accept(sound);
 
         if (tickerView != null) tickerView.visitUris(visitor);
